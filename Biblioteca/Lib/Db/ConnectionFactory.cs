30bb6019dface27bae672424b0903e2de304878a
Milena <bragancamilena@gmail.com>
2014-10-20 10:49:14 -0400

    Projeto incluido

---
+++ Biblioteca/Lib/Db/ConnectionFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Biblioteca.Lib.Db
+{
+    /// <summary>
+    /// Classe responsével por controlar as conexões do sistemas
+    /// com o banco de dados.
+    /// </summary>
+    public class ConnectionFactory
+    {
+        /// <summary>
+        /// Define strig de conexão com o banco
+        /// </summary>
+        private static string strConnection = "server=localhost; uid=biblioteca; pwd=biblioteca; database=biblioteca;";
+        /// <summary>
+        /// Estabelece ema conexão com o banco de dados
+        /// </summary>
+        /// <returns> intancia de uma conexão com o banco</returns>
+        public static IDbConnection GetConnection()
+        {
+            IDbConnection conn = null;
+            conn = new MySqlConnection(strConnection);
+            return conn;
+        }
+    }
+}
---
+++ Biblioteca/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Biblioteca.Lib.Db;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Biblioteca
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IDbConnection conn = ConnectionFactory.GetConnection();
+            conn.Open();
+
+            IDbCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM livro";
+            
+            IDataReader reader = cmd.ExecuteReader();
+            while(reader.Read()){
+                System.Console.WriteLine(reader["titulo"]);
+            }
+        }
+    }
+}
